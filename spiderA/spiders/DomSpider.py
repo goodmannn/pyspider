38752e05b0fcc7843ec8366281b3b36913e06d70
goodmannn <isLujian@163.com>
2018-06-19 22:56:56 +0800

    spidersddd

--- spiderA/spiders/DomSpider.py
+++ spiderA/spiders/DomSpider.py
@@ -1,21 +1,20 @@
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.selector import Selector
+import scrapy
 
 
-class DomzSpider(CrawlSpider):
-    name = "domz"
-    allowed_domains = ["list.jd.com"]
-    start_urls = ["https://list.jd.com/list.html?cat=670,671,1105&page=2&sort=sort_totalsales15_desc&trans=1&JL=6_0_0#J_main"]
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
 
-    rules = [
-        Rule(SgmlLinkExtractor(allow='&sort=sort_totalsales15_desc&trans=1&JL=6_0_0#J_main', restrict_xpaths='//a[@class="pn-next"]'),
-             callback='parse_item', follow=True)
-    ]
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        sel = Selector(response)
-        blog_url = str(response.url)
-        blog_name = sel.xpath('//div[@id="asideHotArticle"]/div/ul/li/a/text()').extract()
-        for x in blog_name:
-            print x
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,20 +1,20 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
+class MySpider(CrawlSpider):
+    name = 'example.com'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/page/2/']
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    rules = (
+        # Extract links matching 'category.php' (but not matching 'subsection.php')
+        # and follow links from them (since no callback means follow=True by default).
+        # Rule(LinkExtractor(allow=('/page/1/', ), deny=('subsection\.php', ))),
 
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(LinkExtractor(allow=('http://quotes.toscrape.com/page/')), callback='parse_item',follow=True),
+    )
 
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+    def parse_item(self, response):
+        print 1111111111111111
